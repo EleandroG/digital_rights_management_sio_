8690b6093665d0b05f25f7b7f1732a3dcfa7e7d6
Eleandro Laureano <eleandrog@ua.pt>
2021-02-23 20:32:21 +0000

    Encrypt & Decrypt

--- server/server.py
+++ server/server.py
@@ -7,11 +7,15 @@
 
 from twisted.web import server, resource
 from twisted.internet import reactor, defer
+from getpass import getpass
+from base64 import b64encode, b64decode
 import logging
 import binascii
 import json
 import os
 import math
+from cryptography.hazmat.primitives.ciphers import Cipher
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import serialization
@@ -42,8 +46,8 @@ CATALOG = { '898a08080d1840793122b7e118b27a95d117ebce':
 CATALOG_BASE = 'catalog'
 CHUNK_SIZE = 1024 * 4
 
-algs = ['AES', 'SEED', 'TripleDES']
-mods = ['CFB', 'CTR', 'OFB']
+algs = ['AES', '3DES']
+mods = ['ECB', 'CFB', 'OFB']
 digest_algorithm =['SHA256', 'SHA512', 'SHA3256']
 
 ciphers = {}
@@ -52,11 +56,11 @@ readings = {}
 users = []
 CSUIT = {}
 
-"""Parameters"""
-p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
-g = 2
-params_numbers = dh.DHParameterNumbers(p,g)
-parameters = params_numbers.parameters(default_backend())
+#"""Parameters"""
+#p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
+#g = 2
+#params_numbers = dh.DHParameterNumbers(p,g)
+#parameters = params_numbers.parameters(default_backend())
 
 
 """This function is used to initialize hmac based on key and user id"""
@@ -72,62 +76,115 @@ def start_hmac(key, who):
         digest = hashes.SHA3_256()
     return hmac.HMAC(key, digest, backend=default_backend())
 
+""" """
+def generate_key(algorithm, salt):
+    password = getpass()
+    password = password.encode()
 
-"""This function is used to initialize the cipher based on a key, iv and a user id"""
-def cipher(key, iv, who):
-    global CSUIT
+    if algorithm == 'AES':
+        length = 16
+    else:
+        length = 24
 
-    alg, mod, dige = CSUIT[who].split("_")
-    if(mod == 'CFB'):
-        mode = modes.CFB(iv)
-    elif(mod == 'CTR'):
-        mode = modes.CTR(iv)
-    elif(mod == 'OFB'):
-        mode = modes.OFB(iv)
-    if(alg == 'AES'):
+    pbkdf = PBKDF2HMAC(salt=salt, algorithm=hashes.SHA256(), iterations=10**5, length=length,
+        backend=default_backend()
+    )
+
+    key = pbkdf.derive(password)
+    return key
+
+"""This function is used to encrypt the data"""
+def encrypt(algorithm, cipherMode, file_to_be_encrypted, file_to_be_saved):
+    salt = os.urandom(16)
+    key = generate_key(algorithm, salt)
+
+    if algorithm == 'AES':
+        blockLength = 16
         algorithm = algorithms.AES(key)
-    elif(alg == 'SEED'):
-        algorithm = algorithms.SEED(key)
-    elif(alg == 'TripleDES'):
+    else:
+        blockLength = 8
         algorithm = algorithms.TripleDES(key)
 
-    ciph = Cipher(algorithm, mode)
-    return ciph
+    iv = None
+    if cipherMode == 'ECB':
+        cipher_mode = modes.ECB(iv)
+    elif cipherMode == 'CFB':
+        cipher_mode = modes.CFB(iv)
+    else:
+        cipher_mode = modes.OFB(iv)
+
+    cipher = Cipher(algorithm, cipher_mode)
+    encryptor = cipher.encryptor()
+
+    encrypted_file = open(file_to_be_encrypted, 'r')
+    saved_file = open(file_to_be_saved, 'wb')
+
+    saved_file.write(b64encode(salt))
+    if iv != None:
+        saved_file.write(b64encode(iv))
+
+    while True:
+        block = encrypted_file.read(blockLength)
+        if block == "":
+            break
+
+        if len(block) != blockLength:
+            break
+
+        block = encryptor.update(block.encode())
+        saved_file.write(b64encode(block))
+        block = block.encode()
+        block = encryptor.update(block)
+
+        saved_file.write(b64encode(block))
+
+        encrypted_file.close()
+        saved_file.close()
 
 
-"""This function is used to encrypt the data based on the user id"""
-def encrypt_data(data, who):
-    global ciphers
 
-    ciphrs = ciphers[who]
-    crypt = ciphrs[1].encryptor()
-    encrypted = crypt.update(data.encode('latin')) + crypt.finalize()
-    crypt = ciphrs[2].copy()
-    crypt.update(encrypted)
-    MAC = crypt.finalize()
-    dict = {'data':encrypted.decode('latin'), 'HMAC':MAC.decode('latin')}
-    crypt = ciphrs[0].encryptor()
-    return crypt.update(json.dumps(dict, indent=4).encode('latin')) + crypt.finalize()
+"""This function is used to decrypt the data"""
+def decrypt(algorithm, cipherMode, file_to_be_decrypted, file_to_be_saved):
+    decrypted_file = open(file_to_be_decrypted, 'rb')
+    saved_file = open(file_to_be_saved, 'w')
 
+    salt = file_to_be_decrypted.read(math.ceil(16 / 3) * 4)
+    salt = b64decode(salt)
+    key = generate_key(algorithm, salt)
 
-"""This function is used to decrypt the data based on the user id"""
-def decrypt_data(data, who):
-    global ciphers
+    if algorithm == 'AES':
+        blockLength = 16
+        algorithm = algorithms.AES(key)
+    else:
+        blockLength = 8
+        algorithm = algorithms.TripleDES(key)
 
-    ciphrs = ciphers[who]
-    dcrypt = ciphrs[0].decryptor()
-    decrypted = dcrypt.update(data) + dcrypt.finalize()
-    decrypted = json.loads(decrypted.decode('latin'))
-    dcrypt = ciphrs[2].copy()
-    dcrypt.update(decrypted['data'].encode('latin'))
-    MAC = dcrypt.finalize()
+    iv = None
+    if cipherMode == 'ECB':
+        cipher_mode = modes.ECB(iv)
+    elif cipherMode == 'CFB':
+        cipher_mode = modes.CFB(iv)
+    else:
+        cipher_mode = modes.OFB(iv)
 
-    if(MAC != decrypted['HMAC'].encode('latin')):
-        return "ERROR 500"
+    cipher = Cipher(algorithm, cipher_mode)
+    decryptor = cipher.decryptor()
+    nextBlock = b64decode(file_to_be_decrypted.read(math.ceil(blockLength / 3) * 4))
 
-    dcrypt = ciphrs[1].decryptor()
-    decrypted = dcrypt.update(decrypted['data'].encode('latin')) + dcrypt.finalize()
-    return decrypted.decode('latin')
+    while True:
+        block = nextBlock
+        nextBlock = b64decode(file_to_be_decrypted.read(math.ceil(blockLength / 3) * 4))
+        block = decryptor.update(block)
+        if nextBlock == b"":
+            break
+        file_to_be_saved.write(block.decode())
+
+        file_to_be_decrypted.close()
+        file_to_be_saved.close()
+
+def diffie_helman_param(key=2048):
+    parameters = dh.generate_parameters(generator=2, key_size=key)
+    return parameters
 
 
 class MediaServer(resource.Resource):
