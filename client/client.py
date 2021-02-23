3304e7d97db8d7e803e1da8be0158796023d5a44
Eleandro Laureano <eleandrog@ua.pt>
2021-02-23 16:38:07 +0000

    Update with old python code

--- client/client.py
+++ client/client.py
@@ -6,16 +6,82 @@ import os
 import subprocess
 import time
 import sys
+import asyncio
 
 logger = logging.getLogger('root')
 FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
 logging.basicConfig(format=FORMAT)
 logger.setLevel(logging.INFO)
 
+
 SERVER_URL = 'http://127.0.0.1:8080'
 
+STATE_CONNECT = 0
+STATE_OPEN = 1
+STATE_DATA = 2
+STATE_CLOSE = 3
+STATE_NEGOTIATE = 4
+STATE_DH = 5
+class Client(asyncio.Protocol):
+    
+    def __init__(self):
+        "Client "
+        self.symetric_ciphers = ["AES", "ChaCha20", "3DES"]
+        self.cipher_modes = ["GCM", "None", "ECB", "CBC"]
+        self.digest_algorithms = ["SHA256", "SHA512", "BLAKE2"]
+
+        self.used_symetric_cyphers = None
+        self.used_cypher_modes = None
+        self.used_digest_algorithms = None
+
+        self.p = None
+        self.g = None
+        self.private_key = None
+        self.shared_key = None
+        self.public_key_pem = None
+
+        self.state = STATE_CONNECT  # Initial State
+        self.buffer = ""  # Buffer to receive data chunks
+
+    def connection_made(self, transport) -> None:
+        self.transport = transport
+
+        logger.debug("Connected to server")
+        message = {
+            "type": "NEGO_REQ", #request to server
+            "algorithms": {
+                "symetric_ciphers": self.symetric_ciphers,
+                "chiper_modes": self.cipher_modes,
+                "digest_algorithms": self.digest_algorithms,
+            },
+        }
+
+        self._send(message)
+
+        self.state = STATE_NEGOTIATE
+
+    def data_received(self, data : str):
+
+        logger.debug("List: ".format(data))
+        try:
+            self.buffer += data.decode()
+        except:
+            logger.exception("Could not decode data from client")
+
+        idx = self.buffer.find("\r\n")
+
+    ...
+
+    def connection_lost(self, exc):
+        logger.info("The connections has been closed by the server")
+        self.loop.stop()
+        
+        
+
+
+
 def main():
-    print("|--------------------------------------|")
+    print("|-------------------------------serve-------|")
     print("|         SECURE MEDIA CLIENT          |")
     print("|--------------------------------------|\n")
 
--- server/server.py
+++ server/server.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
-from twisted.web import server, resource
-from twisted.internet import reactor, defer
+"""
+83069 - Eleandro Laureano
+78444 - Nuno Matamba
+"""
+import base64
 import logging
 import binascii
 import json
 import os
 import math
+import sys
+from twisted.web import server, resource
+from twisted.internet import reactor, defer
+
+
+#TODO: Arranjar o import
+#sys.path.insert(1, '../client/s')
+#import secure_functions
 
 logger = logging.getLogger('root')
 FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
@@ -27,10 +38,36 @@ CATALOG = { '898a08080d1840793122b7e118b27a95d117ebce':
 CATALOG_BASE = 'catalog'
 CHUNK_SIZE = 1024 * 4
 
+
 class MediaServer(resource.Resource):
+
+    def __init__(self, signal):
+        self.signal = signal
+        #self.state = STATE_CONNECT
+        self.file = None
+        self.file_name = None
+        self.file_path = None
+        #self.storage_dir = storage_dir
+        self.buffer = ""
+        self.peername = ""
+
+        self.ciphers = ["AES", "3DES", "ChaCha20"]
+        self.cipher_modes = ["ECB", "CBC", "GCM", "None"]
+        self.digest_algorithms = ["SHA256", "SHA512", "BLAKE2"]
+
+        self.used_cipher = None
+        self.used_cipher_mode = None
+        self.used_digest_algorithm = None
+
+        self.p = None
+        self.g = None
+        self.private_key = None
+        self.shared_key = None
+        self.public_key_pem = None
+
     isLeaf = True
 
-    # Send the list of media files to clients
+    """This function sends the list of media files to clients"""
     def do_list(self, request):
 
         #auth = request.getHeader('Authorization')
@@ -39,7 +76,7 @@ class MediaServer(resource.Resource):
         #    return 'Not authorized'
 
 
-        # Build list
+        #Build list
         media_list = []
         for media_id in CATALOG:
             media = CATALOG[media_id]
@@ -51,37 +88,41 @@ class MediaServer(resource.Resource):
                 'duration': media['duration']
                 })
 
-        # Return list to client
+        #Return list to client
         request.responseHeaders.addRawHeader(b"content-type", b"application/json")
         return json.dumps(media_list, indent=4).encode('latin')
 
 
-    # Send a media chunk to the client
+    """This function sends a media chunk to the client"""
     def do_download(self, request):
+
+
+
+
         logger.debug(f'Download: args: {request.args}')
         
         media_id = request.args.get(b'id', [None])[0]
         logger.debug(f'Download: id: {media_id}')
 
-        # Check if the media_id is not None as it is required
+        #Check if the media_id is not None as it is required
         if media_id is None:
             request.setResponseCode(400)
             request.responseHeaders.addRawHeader(b"content-type", b"application/json")
             return json.dumps({'error': 'invalid media id'}).encode('latin')
         
-        # Convert bytes to str
+        #Convert bytes to str
         media_id = media_id.decode('latin')
 
-        # Search media_id in the catalog
+        #Search media_id in the catalog
         if media_id not in CATALOG:
             request.setResponseCode(404)
             request.responseHeaders.addRawHeader(b"content-type", b"application/json")
             return json.dumps({'error': 'media file not found'}).encode('latin')
         
-        # Get the media item
+        #Get the media item
         media_item = CATALOG[media_id]
 
-        # Check if a chunk is valid
+        #Check if a chunk is valid
         chunk_id = request.args.get(b'chunk', [b'0'])[0]
         valid_chunk = False
         try:
@@ -100,7 +141,7 @@ class MediaServer(resource.Resource):
 
         offset = chunk_id * CHUNK_SIZE
 
-        # Open file, seek to correct position and return the chunk
+        #Open file, seek to correct position and return the chunk
         with open(os.path.join(CATALOG_BASE, media_item['file_name']), 'rb') as f:
             f.seek(offset)
             data = f.read(CHUNK_SIZE)
@@ -114,11 +155,12 @@ class MediaServer(resource.Resource):
                     },indent=4
                 ).encode('latin')
 
-        # File was not open?
+        #File was not open?
         request.responseHeaders.addRawHeader(b"content-type", b"application/json")
         return json.dumps({'error': 'unknown'}, indent=4).encode('latin')
 
-    # Handle a GET request
+
+    """This function handles a GET request"""
     def render_GET(self, request):
         logger.debug(f'Received request for {request.uri}')
 
@@ -143,12 +185,171 @@ class MediaServer(resource.Resource):
             request.setResponseCode(500)
             request.responseHeaders.addRawHeader(b"content-type", b"text/plain")
             return b''
-    
-    # Handle a POST request
+
+
+    #Handle a POST request
     def render_POST(self, request):
         logger.debug(f'Received POST for {request.uri}')
         request.setResponseCode(501)
         return b''
+
+
+    """1. This function processes a negotiation with the client and server"""
+    def negotiation(self, media_titles: str):
+        logger.debug(f"Process Negotation: {media_titles}")
+
+        ciphers = media_titles["algorithms"]["ciphers"]
+        cipher_modes = media_titles["algorithms"]["cipher_modes"]
+        digest_algorithms = media_titles["algorithms"]["digest_algorithms"]
+
+        for cipher in ciphers:
+            if cipher in self.ciphers:
+                self.used_cipher = cipher
+                break
+
+        for cipher_md in cipher_modes:
+            if cipher_md in self.cipher_modes:
+                self.used_cipher_mode = cipher_md
+                break
+
+        for digest_alg in digest_algorithms:
+            if digest_alg in self.digest_algorithms:
+                self.used_digest_algorithm = digest_alg
+                break
+
+        media_titles = {
+            "type": "NEGO_REP",
+            "algorithms": {
+                "_cipher": self.used_cipher,
+                "cipher_mode": self.used_cipher_mode,
+                "digest_algorithm": self.used_digest_algorithm,
+            },
+        }
+
+        if (
+            self.used_cipher is not None
+            and self.used_chiper_mode is not None
+            and self.used_digest_algorithm is not None
+        ):
+            self._send(media_titles)
+            return True
+
+        return False
+
+    """2. This function negotiate ephemeral keys between the client and server using Diffie-Hellman"""
+    def negotiate_keys(self, media_titles: str):
+        self.p = media_titles["parameters"]["p"]
+        self.g = media_titles["parameters"]["g"]
+        public_key_pem_client = bytes(media_titles["parameters"]["public_key"], "ISO-8859-1")
+
+        try:
+            self.private_key, self.public_key_pem = secure_funtions.diffie_hellman_server(
+                self.p, self.g, public_key_pem_client
+            )
+
+            media_titles = {
+                "type": "DH_SERVER_KEY",
+                "key": str(self.public_key_pem, "ISO-8859-1"),
+            }
+
+            self._send(media_titles)
+
+            self.shared_key = secure_funtions.generate_shared_key(
+                self.private_key, public_key_pem_client, self.used_digest_algorithm
+            )
+
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    """4. This function is used to validate the integrity of all messages and chunks"""
+    def validate_integrity(self, request, frame: str) -> None:
+        #Verify integrity of all messages
+        try:
+            message = json.loads(frame) #TODO: Mudar o nome de "frame" para outra coisa.
+        except:
+            logger.exception("Could not decode JSON message: {}".format(frame)) #TODO: Mudar mensagem de erro
+            self.transport.close()
+            return
+        mtype = message.get("type", "").upper()
+
+        if mtype == "MEDIA_FILES":
+            actual_message = base64.b64decode(message["payload"])
+            mac = base64.b64decode(message["mac"])
+            if message["iv"] != None:
+                iv = base64.b64decode(message["iv"])
+            else:
+                iv = None
+            if message["nonce"] != None:
+                nonce = base64.b64decode(message["nonce"])
+            else:
+                nonce = None
+            if message["tag"] != None:
+                tag = base64.b64decode(message["tag"])
+            else:
+                tag = None
+
+            digest = secure_functions.mac_generator(
+                actual_message, self.shared_key, self.used_digest_algorithm
+            )
+            if mac != digest:
+                if self.file_path != None:  #If we created a file delete it!
+                    os.remove(self.file_path)
+                logger.warning("The integrity of this message has been compromised")
+                ret = False
+            else:
+                actual_message = secure_functions.symmetric_decryptor(
+                    actual_message,
+                    self.shared_key,
+                    self.used_symetric_cipher,
+                    self.used_chiper_mode,
+                    iv,
+                    nonce,
+                    tag,
+                )
+
+                actual_message = actual_message.decode()
+                actual_message = actual_message.split("}")[0] + "}"
+                message = json.loads(actual_message)
+                mtype = message["type"]             #TODO: Como resolver o "mtype" ?
+
+        elif mtype == "NEGO_REP":
+            ret = self.process_negotiation(message)
+        else:
+            logger.warning("Invalid message type: {}".format(message["type"]))
+            ret = False
+
+        if not ret:
+            try:
+                self._send({"type": "ERROR", "message": "See server"})
+            except:
+                pass  #Silently ignore
+
+            logger.info("Closing transport")
+            if self.file is not None:
+                self.file.close()
+                self.file = None
+
+        #Verify integrity of chunks
+        #Check if a chunk is valid
+        chunk_id = request.args.get(b'chunk', [b'0'])[0]
+        media_id = request.args.get(b'id', [None])[0]
+        valid_chunk = False
+        media_item = CATALOG[media_id]
+        try:
+            chunk_id = int(chunk_id.decode('latin'))
+            if chunk_id >= 0 and chunk_id < math.ceil(media_item['file_size'] / CHUNK_SIZE):
+                valid_chunk = True
+        except:
+            logger.warn("Chunk format is invalid")
+
+        if not valid_chunk:
+            request.setResponseCode(400)
+            request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+            return json.dumps({'error': 'invalid chunk id'}).encode('latin')
+
+        logger.debug(f'Chunk: {chunk_id} was sent')
 
 
 print("Server started")
